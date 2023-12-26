df67e79fed103c55f62599009dc293db91ff1274
alexa <letsiki@gmail.com>
2023-12-26 02:09:45 +0200

    v1 working

--- color.py
+++ color.py
@@ -3,12 +3,14 @@ import mixbox
 
 class Color:
 
-    def __init__(self, rgb):
-        self.R = rgb[0]
-        self.G = rgb[1]
-        self.B = rgb[2]
-
+    def __init__(self, r, g, b):
+        self.R = r
+        self.G = g
+        self.B = b
     def __add__(self, other):
-        return Color(mixbox.lerp((self.R, self.G, self.B), (other.R, other.G, other.B), t=0.5))
+        mixed_color = mixbox.lerp((self.R, self.G, self.B), (other.R, other.G, other.B), t=0.5)
+        return Color(mixed_color[0], mixed_color[1], mixed_color[2])
 
+    def __str__(self):
+        return '#{:02x}{:02x}{:02x}'.format(self.R, self.G, self.B)
 
--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from color import Color
 
 
 def update_colors(event):
-    color_left = (r1.get(), g1.get(), b1.get())
-    canvas_left.config(bg='#%02X%02X%02X' % color_left)
-    color_right = (r2.get(), g2.get(), b2.get())
-    canvas_right.config(bg='#%02X%02X%02X' % color_right)
-    color_bottom = Color(color_left) + Color(color_right)
-    canvas_bottom.config(bg='#%02X%02X%02X' % (color_bottom.R, color_bottom.G, color_bottom.B))
+    color_left = Color(r1.get(), g1.get(), b1.get())
+    canvas_left.config(bg=str(color_left))
+    color_right = Color(r2.get(), g2.get(), b2.get())
+    canvas_right.config(bg=str(color_right))
+    color_bottom = color_left + color_right
+    canvas_bottom.config(bg=str(color_bottom))
 
 
 window = Tk()
@@ -29,8 +29,8 @@ b2 = Scale(window, from_=0, to=255, orient=HORIZONTAL, command=update_colors)
 canvas_left = Canvas()
 canvas_right = Canvas()
 canvas_bottom = Canvas()
-canvas_left.config(width=180, height=180, bg=""'#%02X%02X%02X' % (r1.get(), g1.get(), b1.get()))
-canvas_right.config(width=180, height=180, bg=""'#%02X%02X%02X' % (r2.get(), g2.get(), b2.get()))
+canvas_left.config(width=180, height=180)
+canvas_right.config(width=180, height=180)
 canvas_bottom.config(width=600, height=140)
 r1.grid(column=1, row=1)
 g1.grid(column=1, row=2)
@@ -41,5 +41,5 @@ b2.grid(column=2, row=3)
 canvas_left.grid(column=0, row=0, rowspan=4)
 canvas_right.grid(column=3, row=0, rowspan=4)
 canvas_bottom.grid(row=4, column=0, columnspan=4)
-
+update_colors("")
 window.mainloop()
